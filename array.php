b114015f8286c790e636a0dead08cff30fc72eea
Falmban Ammar <devAspirant@gmail.com>
2020-04-11 01:33:37 +0300

    Adding associate array

--- array.php
+++ array.php
@@ -5,4 +5,14 @@ $index_array = ["ammar","AMMAR","Abulhalem"];
 
 echo $index_array[1];
 
+// ترتيب المصفوفات
+print_r(sort($index_array));
+
+
+
+// تتميز بالمرونة والقدرة على التكوين الإضافي assositate arrat 
+$assosicate_array = ["Name"=>["Ammar","ammar"]];
+
+print_r($assosicate_array["Name"]);
+
 ?>
